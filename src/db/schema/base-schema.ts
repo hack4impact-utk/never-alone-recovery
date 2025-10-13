202620128097be1655445220905e455464e6d3e9
Trishu Patel <pateltrishu28@gmail.com>
2025-10-13 18:49:56 -0400

    refactor: Change base schema to allow for type inference

--- src/db/schema/base-schema.ts
+++ src/db/schema/base-schema.ts
@@ -1,21 +1,14 @@
 import { text, timestamp } from "drizzle-orm/pg-core";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function withBaseColumns<T extends Record<string, any>>(
-  customColumns: T,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-): Record<string, any> {
-  return {
-    id: text("id")
-      .primaryKey()
-      .$defaultFn(() => crypto.randomUUID()),
-    createdDate: timestamp("createdDate").defaultNow().notNull(),
-    createdBy: text("createdBy"),
-    editedDate: timestamp("editedDate")
-      .defaultNow()
-      .notNull()
-      .$onUpdate(() => new Date()),
-    editedBy: text("editedBy"),
-    ...customColumns,
-  };
-}
+export const baseSchema = {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  createdDate: timestamp("createdDate").defaultNow().notNull(),
+  createdBy: text("createdBy"),
+  editedDate: timestamp("editedDate")
+    .defaultNow()
+    .notNull()
+    .$onUpdate(() => new Date()),
+  editedBy: text("editedBy"),
+};
--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,16 +1,14 @@
 import { pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
-import { withBaseColumns } from "./base-schema";
+import { baseSchema } from "./base-schema";
 
 export const roleEnum = pgEnum("role", ["disabled", "staff", "admin"]);
 
-export const users = pgTable(
-  "user",
-  withBaseColumns({
-    name: text("name"),
-    email: text("email").unique(),
-    emailVerified: timestamp("emailVerified", { mode: "date" }),
-    image: text("image"),
-    role: roleEnum("role").default("disabled").notNull(),
-  }),
-);
+export const users = pgTable("user", {
+  ...baseSchema,
+  name: text("name"),
+  email: text("email").unique(),
+  emailVerified: timestamp("emailVerified", { mode: "date" }),
+  image: text("image"),
+  role: roleEnum("role").default("disabled").notNull(),
+});
