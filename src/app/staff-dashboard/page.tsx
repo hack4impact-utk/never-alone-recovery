c65aded0781ff634b4fe68dbf6c7f3b050879c46
Phil Marino <philipmarino1@gmail.com>
2025-10-27 16:55:10 -0400

    created MUI DG and implemented sorting by LName

--- src/app/staff-dashboard/page.tsx
+++ src/app/staff-dashboard/page.tsx
@@ -1,5 +1,61 @@
+"use client";
 import { Box } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import { ReactNode } from "react";
+
+const columns = [
+  {
+    field: "firstName",
+    headerName: "First Name",
+    width: 150,
+    editable: false,
+    sortable: false,
+  },
+
+  {
+    field: "lastName",
+    headerName: "Last Name",
+    width: 150,
+    editable: false,
+    sortable: false,
+  },
+
+  {
+    field: "email",
+    headerName: "Email",
+    width: 150,
+    editable: false,
+    sortable: false,
+  },
+
+  {
+    field: "role",
+    headerName: "Role",
+    width: 150,
+    editable: true,
+    sortable: false,
+    type: "singleSelect",
+    valueOptions: ["Admin", "Staff", "Disabled"],
+  },
+];
+
+// sample for now
+const rows = [
+  {
+    id: 1,
+    firstName: "John",
+    lastName: "Doe",
+    email: "john.doe@company.com",
+    role: "Manager",
+  },
+  {
+    id: 2,
+    firstName: "John",
+    lastName: "Aoe",
+    email: "john.doe@company.com",
+    role: "Manager",
+  },
+];
 
 export default function StaffDashboardPage(): ReactNode {
   return (
@@ -12,7 +68,25 @@ export default function StaffDashboardPage(): ReactNode {
         alignItems: "center",
       }}
     >
-      Staff Dashboard
+      <Box sx={{ height: "80%", width: "90%", maxWidth: 1200 }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 10,
+              },
+            },
+            sorting: {
+              sortModel: [{ field: "lastName", sort: "asc" }],
+            },
+          }}
+          pageSizeOptions={[5, 10, 25]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
     </Box>
   );
 }
