435d789036b4d87b253366be96afe038426db512
Philip Marino <philipmarino1@gmail.com>
2025-11-03 11:26:23 -0500

    more prettier formatting fixes

--- src/app/staff-dashboard/page.tsx
+++ src/app/staff-dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import { ReactNode } from "react";
+
 import { getAllStaff } from "@/api/staff";
 import StaffTable from "@/components/staff/staff-table";
 
