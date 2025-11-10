ef0aeea84a5d2e471960d8252874677ef514d349
Trishu Patel <pateltrishu28@gmail.com>
2025-11-09 23:08:28 -0500

    feat: Add search bar

--- src/components/tasks/client-list/index.tsx
+++ src/components/tasks/client-list/index.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { Box, ToggleButton, ToggleButtonGroup } from "@mui/material";
+import ClearIcon from "@mui/icons-material/Clear";
+import Search from "@mui/icons-material/Search";
+import {
+  Box,
+  IconButton,
+  TextField,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
 import { ReactNode, useState } from "react";
 
 import { ClientTasks } from "@/types/client-tasks";
@@ -11,10 +19,34 @@ type ClientTaskListProps = {
   clientTasks: ClientTasks[];
 };
 
+const getFilteredClientTasks = (
+  clientTasks: ClientTasks[],
+  searchQuery: string,
+  showCompletedTasks: boolean,
+): ClientTasks[] => {
+  return clientTasks
+    .filter((client) =>
+      client.tasks.some((task) => task.completed === showCompletedTasks),
+    )
+    .filter(
+      (client) =>
+        client.firstName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        client.lastName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        client.email.toLowerCase().includes(searchQuery.toLowerCase()),
+    );
+};
+
 export default function ClientList({
   clientTasks,
 }: ClientTaskListProps): ReactNode {
   const [showCompletedTasks, setShowCompletedTasks] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredClientTasks = getFilteredClientTasks(
+    clientTasks,
+    searchQuery,
+    showCompletedTasks,
+  );
 
   return (
     <Box
@@ -28,18 +60,49 @@ export default function ClientList({
       <Box
         sx={{ display: "flex", justifyContent: "space-between", mb: "1rem" }}
       >
+        <Box display="flex" alignItems="center">
+          <TextField
+            id="search-bar"
+            className="text"
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+            placeholder="Search..."
+            size="small"
+            sx={{ width: "450px" }}
+            slotProps={{
+              input: {
+                endAdornment: (
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                    {searchQuery && (
+                      <IconButton
+                        onClick={() => setSearchQuery("")}
+                        edge="end"
+                        size="small"
+                      >
+                        <ClearIcon />
+                      </IconButton>
+                    )}
+                  </Box>
+                ),
+              },
+            }}
+          />
+          <Search sx={{ fontSize: 35, ml: 1 }} color="primary" />
+        </Box>
         <ToggleButtonGroup
           value={showCompletedTasks}
           color="primary"
           exclusive
-          onChange={(e, newStatus) => setShowCompletedTasks(newStatus)}
+          onChange={() => setShowCompletedTasks(!showCompletedTasks)}
         >
           <ToggleButton value={false}>TODO</ToggleButton>
           <ToggleButton value={true}>Completed</ToggleButton>
         </ToggleButtonGroup>
       </Box>
       <Box>
-        {clientTasks
+        {filteredClientTasks
           .filter((client) =>
             client.tasks.some((task) => task.completed === showCompletedTasks),
           )
