5576b99493ed26d3635d579ce64c38325ff82026
Trishu Patel <pateltrishu28@gmail.com>
2025-11-11 14:48:50 -0500

    chore: Add client, user, and audit database relations

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,5 +1,7 @@
+import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
 
+import { audits } from "./audit";
 import { baseSchema } from "./base-schema";
 import { staffRoleEnum } from "./enum";
 
@@ -11,3 +13,7 @@ export const users = pgTable("user", {
   image: text("image").notNull(),
   role: staffRoleEnum("role").default("disabled").notNull(),
 });
+
+export const userRelations = relations(users, ({ many }) => ({
+  audits: many(audits),
+}));
