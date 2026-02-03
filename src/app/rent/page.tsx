727d390216942b740e5653ee1c1592aa62d65026
amy-huang16 <ahuang16@vols.utk.edu>
2026-02-02 23:20:54 -0500

    finish migration

--- src/app/rent/page.tsx
+++ src/app/rent/page.tsx
@@ -1,7 +1,7 @@
 import { Box, Typography } from "@mui/material";
 import { ReactNode } from "react";
 
-import { getAllClientBalances } from "@/api/balances";
+import { getAllClientBalances } from "@/api/rent/queries";
 import RentTable from "@/components/rent/rent-table";
 
 export default async function RentPage(): Promise<ReactNode> {
