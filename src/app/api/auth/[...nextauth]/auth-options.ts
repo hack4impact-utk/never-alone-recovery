33f0bd6a8d7fcd444fb83e197d684c1d60c97a7e
Trishu Patel <pateltrishu28@gmail.com>
2026-02-15 14:09:33 -0500

    feat: Add new user columns to JWT

--- src/app/api/auth/[...nextauth]/auth-options.ts
+++ src/app/api/auth/[...nextauth]/auth-options.ts
@@ -16,6 +16,27 @@ const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        const dbUser = await db.query.users.findFirst({
+          where: (users, { eq }) => eq(users.id, user.id),
+        });
+
+        if (dbUser) {
+          token.user = dbUser;
+        }
+      }
+
+      return token;
+    },
+    session({ session, token }) {
+      if (token.user) {
+        session.user = token.user;
+      }
+      return session;
+    },
+  },
 };
 
 export default authOptions;
