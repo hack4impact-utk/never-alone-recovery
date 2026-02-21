347b0211fa794fced591137c5e517f146a756c39
wafflesLL <lloeffel@vols.utk.edu>
2026-02-21 00:02:46 -0500

    fixed for format checks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import { ReactNode } from "react";
@@ -10,6 +10,7 @@ import DateLocalizationProvider from "@/providers/date-localization-provider";
 import NextAuthProvider from "@/providers/next-auth-provider";
 import NotistackProvider from "@/providers/notistack-provider";
 import theme from "@/styles/theme";
+
 import Header from "../components/header";
 import authOptions from "./api/auth/[...nextauth]/auth-options";
 
