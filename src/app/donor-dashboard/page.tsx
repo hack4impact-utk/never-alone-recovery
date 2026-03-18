10ac6448aaf534422fd3214401372db9677033ea
Trishu Patel <pateltrishu28@gmail.com>
2026-03-18 14:52:05 -0400

    feat: Add donation page

---
+++ src/app/donor-dashboard/page.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import { ReactNode } from "react";
+
+export default function DonorDashboard(): ReactNode {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      Donor Dashboard
+    </Box>
+  );
+}
