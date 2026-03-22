6d14fbd9cacec1732cfb0499d46b646db56679c4
wafflesLL <lloeffel@vols.utk.edu>
2026-03-21 20:56:34 -0400

    added addAudit endpoints for client_staff_changed and staff_role_changed

--- src/api/staff/public-mutations.ts
+++ src/api/staff/public-mutations.ts
@@ -6,6 +6,8 @@ import { Result } from "@/types/result";
 import { User } from "@/types/schema";
 import getUserSession from "@/utils/auth/get-user-session";
 
+import { addAudit } from "../audit/private-mutations";
+
 export async function updateStaffRole(
   user: User,
   role: StaffRole,
@@ -19,6 +21,19 @@ export async function updateStaffRole(
   if (session.user.email === user.email) {
     return [null, "Forbidden"];
   }
+  
+  const [, updateStaffError] = await updateStaff({...user, role: role});
+
+  if (!updateStaffError){
+    return [null, "Error updating role"];
+  }
+
+  await addAudit({
+    staffId: session.user.id,
+    clientId: null,
+    type: "staff_role_changed",
+    message: `Staff role updated to ${role}`,
+  });
 
   return await updateStaff({ ...user, role });
 }
