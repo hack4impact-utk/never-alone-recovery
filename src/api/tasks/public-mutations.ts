7e699430c1f538295e92eda45a484db23baeb75c
Liam Loeffel <lloeffel@vols.utk.edu>
2026-03-29 17:07:31 -0400

    fix(staff): added addAudit endpoints for client_staff_changed and staff_role_changed (#84)

--- src/api/tasks/public-mutations.ts
+++ src/api/tasks/public-mutations.ts
@@ -2,10 +2,29 @@
 
 import { Result } from "@/types/result";
 import { Task } from "@/types/schema";
+import getUserSession from "@/utils/auth/get-user-session";
 
+import { addAudit } from "../audit/private-mutations";
 import { updateTask } from "./private-mutations";
 
 export async function markTaskAsCompleted(task: Task): Promise<Result<Task>> {
+  const session = await getUserSession();
+
+  if (!session) {
+    return [null, "Unauthorized"];
+  }
+
+  const [, error] = await updateTask({ ...task, completed: true });
+
+  if (!error) {
+    await addAudit({
+      staffId: session.user.id,
+      clientId: task.clientId,
+      type: "client_task_completed",
+      message: `${task.description} completed`,
+    });
+  }
+
   return await updateTask({ ...task, completed: true });
 }
 
