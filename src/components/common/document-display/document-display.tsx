c75620262d7fe8d2aecf6306f2ba2ea85033cad3
Trishu Patel <pateltrishu28@gmail.com>
2026-04-05 20:40:57 -0400

    feat: Add loading to document display

--- src/components/common/document-display/document-display.tsx
+++ src/components/common/document-display/document-display.tsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material";
-import { JSX } from "react";
+import { Box, CircularProgress } from "@mui/material";
+import { JSX, useEffect, useState } from "react";
 
 type DocumentDisplayProps = {
   pdfUrl: string;
@@ -8,11 +8,34 @@ type DocumentDisplayProps = {
 export default function DocumentDisplay({
   pdfUrl,
 }: DocumentDisplayProps): JSX.Element {
+  const [src, setSrc] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setSrc(pdfUrl);
+  }, [pdfUrl]);
+
   return (
     <Box
-      sx={{ height: { xs: "400px", sm: "500px", md: "750px" }, width: "100%" }}
+      sx={{
+        height: { xs: "400px", sm: "500px", md: "750px" },
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      <iframe src={`${pdfUrl}#toolbar=0`} width="100%" height="100%" />
+      {isLoading && <CircularProgress />}
+
+      <iframe
+        src={`${src}#toolbar=0`}
+        width="100%"
+        height="100%"
+        loading="lazy"
+        onLoad={() => setIsLoading(false)}
+        hidden={isLoading}
+      />
     </Box>
   );
 }
